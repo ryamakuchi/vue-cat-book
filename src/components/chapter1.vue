878df3e2e31e3122ab0764c9e64afafe16437430
ryamakuchi <r.yamakuchi@gmail.com>
2018-09-12 00:08:12 +0900

    chapter2からは、Vuerouterでページごとに分けて作る

---
+++ src/components/chapter1.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="chapter1">
+    {{ message }}
+    <ol>
+      <li v-for="item in list"> {{ item }} </li>
+    </ol>
+    <button v-on:click="handleClick">Click</button>
+    <input v-model="message">
+    <br>
+    <button v-on:click="show=!show">切り替え</button>
+    <transition>
+      <p v-if="show">切り替えられるよ</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'chapter1',
+  data () {
+    return {
+      message: '初期メッセージ',
+      list: ['りんご', 'ばなな', 'いちご'],
+      show: true
+    }
+  },
+  methods: {
+    handleClick: function (event) {
+      alert(event.target)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .v-enter-active, .v-leave-active {
+    transition: opacity 1s;
+  }
+  .v-enter, .v-leave-to {
+    opacity: 0;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import chapter1 from '@/components/chapter1'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/chapter1',
+      name: 'chapter1',
+      component: chapter1
     }
   ]
 })
